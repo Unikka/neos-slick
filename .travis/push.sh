a8f9c08a6d3a8987da1f6ebd00e8e322a6521b59
Markus Günther <markus.guenther@noerdisch.de>
2018-09-14 22:08:25 +0200

    TASK: Improve travis back push

--- .travis/push.sh
+++ .travis/push.sh
@@ -6,9 +6,11 @@ setup_git() {
 }
 
 commit_public_resources() {
+  git remote add origin-travis https://${GH_TOKEN:-git}@github.com/markusguenther/neos-slick.git
+  git checkout -b origin-travis/master
   git add ./Resources/Public
   git commit --message "Build: Adding public resources for BUILD $TRAVIS_BUILD_NUMBER"
-  git push origin HEAD:master
+  git push origin-travis HEAD:master
 }
 
 copy_node_modules_files() {
