790ac6a7dec9e8ee64aaf2b6e1bda47ba514304b
Markus Günther <markus.guenther@noerdisch.de>
2018-09-14 21:43:00 +0200

    BUILD: Automate resources handling
    Copies the node_modules files to the public resources and commit
    the change to git.

---
+++ .travis/push.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "markus.guenther@noerdisch.de"
+  git config --global user.name "Markus Günther"
+}
+
+commit_public_resources() {
+  git add . *
+  git commit --message "Build: Adding public resources for BUILD $TRAVIS_BUILD_NUMBER"
+  git push
+}
+
+copy_node_modules_files() {
+  yarn copy
+}
+
+setup_git
+copy_node_modules_files
+commit_public_resources
