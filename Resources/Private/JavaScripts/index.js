10d327230d12a19edaf4c1f378ace079997a3eb4
Markus Günther <info@unikka.de>
2019-10-24 09:53:53 +0200

    New: Adjust the height of the inner slider content
    The previous version always needed CSS adjustments before the slider
    was rendered correct. The reason was the inner content div.
    
    We now adjust the width automatically and therefore the user don`t need
    to adjust the own css anymore.

--- Resources/Private/JavaScripts/index.js
+++ Resources/Private/JavaScripts/index.js
@@ -1,4 +1,33 @@
 import $ from 'jquery';
 import 'slick-carousel';
 
-$('[data-slick]').slick();
+const adjustDimensions = slick => {
+    const width = parseInt(slick.listWidth);
+    const slides = Array.from(slick.$slides);
+    slides.forEach(slide => {
+        const innerSlide = slide.querySelector('.slide__inner');
+        if (!isNaN(width) && innerSlide !== undefined && innerSlide !== null) {
+            innerSlide.style.width = width + 'px';
+        }
+    });
+};
+
+const sliders = $('[data-slick]');
+sliders.on('setPosition', function(event, slick) {
+    adjustDimensions(slick);
+});
+
+sliders.on('init', function(event, slick) {
+    adjustDimensions(slick);
+});
+
+sliders.on('reinit', function(event, slick) {
+    adjustDimensions(slick);
+});
+
+Array.from(sliders).forEach(slider => {
+    const configuration = slider.getAttribute('data-slick');
+    const configurationObject =
+        configuration != '' ? JSON.parse(configuration) : {};
+    $(slider).slick(configurationObject);
+});
