eb967be564f18334939b0dba9c9597ad94a37dd4
Markus Günther <info@unikka.de>
2019-10-24 21:38:51 +0200

    Fix: Remove empty slides in neos backend
    In the backend each slide has an empty slide as a follower

--- Resources/Private/JavaScripts/index.js
+++ Resources/Private/JavaScripts/index.js
@@ -12,6 +12,18 @@ const adjustDimensions = slick => {
     });
 };
 
+const removeEmptySlides = slick => {
+    // use reverse to prevent index changes when removing slides
+    const slides = Array.from(slick.$slides).reverse();
+    slides.forEach(slide => {
+        const hasInnerSlide = slide.querySelector('.slide__inner');
+        if (hasInnerSlide === null) {
+            const slickIndex = slide.getAttribute('data-slick-index');
+            slick.slickRemove(slickIndex);
+        }
+    });
+};
+
 const sliders = $('[data-slick]');
 sliders.on('setPosition', function(event, slick) {
     adjustDimensions(slick);
@@ -19,6 +31,11 @@ sliders.on('setPosition', function(event, slick) {
 
 sliders.on('init', function(event, slick) {
     adjustDimensions(slick);
+    if ('neos' in window) {
+        // For some reason we have in the neos backend for each slide one additional
+        // empty slide. This is bit hacky way get rid of them.
+        removeEmptySlides(slick);
+    }
 });
 
 sliders.on('reinit', function(event, slick) {
